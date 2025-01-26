6e6846f2473b3e7b9fea34bfc593dd85de0d28ae
BlessingLuphahla <luphahlablessingthamsanqa@gmail.com>
2025-01-26 13:03:32 +0200

    files update

---
+++ src/assets/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PersonIcon from '@mui/icons-material/Person';
+function Home() {
+  return (
+    <div>
+        this is home page
+        <PersonIcon></PersonIcon>
+    </div>
+  )
+}
+
+export default Home
