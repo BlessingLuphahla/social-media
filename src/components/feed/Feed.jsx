18bf2794a79dee756e37fae72b06aa8ffd8a3404
BlessingLuphahla <luphahlablessingthamsanqa@gmail.com>
2025-01-27 09:51:13 +0200

    feed update

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import './feed.css'
+import React from "react";
+import "./feed.css";
 
 function Feed() {
   return (
-    <div className='feed'
-    >Feed</div>
-  )
+    <div className="feed">
+      <div className="feedWrapper">
+        <div className="feedTitle">
+          <h4>Home</h4>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Feed
+export default Feed;
---
+++ src/components/share/Share.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Share() {
+  return (
+    <div>Share</div>
+  )
+}
+
+export default Share
+
