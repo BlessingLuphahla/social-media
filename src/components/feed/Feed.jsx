df54702306ce8a8e73c158022e31c3ab5068c870
BlessingLuphahla <luphahlablessingthamsanqa@gmail.com>
2025-01-27 14:51:13 +0200

    feed update

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./feed.css";
+import Share from "../share/Share";
 
 function Feed() {
   return (
     <div className="feed">
       <div className="feedWrapper">
+        <Share></Share>
         <div className="feedTitle">
           <h4>Home</h4>
         </div>
--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import "./share.css"
+import profilePic from "../../assets/images/person/apollo.jpg"
 
 function Share() {
   return (
-    <div>Share</div>
+    <div className="share">
+        <div className="shareWrapper">
+            <div className="shareTop">
+                <img className="shareProfileImg" src={profilePic} alt="" />
+                <input placeholder="What's in your mind Redd?" className="shareInput" />
+            </div>
+            <hr className="shareHr" />
+            <div className="shareBottom">
+                <div className="shareOptions">
+                    <div className="shareOption">
+                        <img src="/assets/images/share/1.jpg" alt="" className="shareIcon" />
+                        <span className="shareOptionText">Photo or Video</span>
+                    </div>
+                    <div className="shareOption">
+                        <img src="/assets/images/share/2.jpg" alt="" className="shareIcon" />
+                        <span className="shareOptionText">Tag</span>
+                    </div>
+                    <div className="shareOption">
+                        <img src="/assets/images/share/3.jpg" alt="" className="shareIcon" />
+                        <span className="shareOptionText">Location</span>
+                    </div>
+                    <div className="shareOption">
+                        <img src="/assets/images/share/4.jpg" alt="" className="shareIcon" />
+                        <span className="shareOptionText">Feelings</span>
+                    </div>
+                </div>
+                <button className="shareButton">Share</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
