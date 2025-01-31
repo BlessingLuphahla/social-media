8dae894672703c77139b1b03b17e7d69da06f15d
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-02-01 00:38:54 +0200

    login update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
-import Login from './assets/pages/login/Login'
+import Profile from './assets/pages/profile/Profile'
+// import Login from './assets/pages/login/Login'
 
 function App() {
   return (
     <div>
-      <Login />
+      <Profile />
     </div>
   )
 }
--- src/assets/pages/login/Login.jsx
+++ src/assets/pages/login/Login.jsx
@@ -12,8 +12,10 @@ function Login() {
         </div>
         <div className="loginRight">
           <div className="loginBox">
+            <input  type="text" placeholder="Username" className="loginInput" />
             <input  type="email" placeholder="Email" className="loginInput" />
             <input type="password" placeholder="Password" className="loginInput" />
+            <input  type="password" placeholder="Password Again" className="loginInput" />
             <button className="loginButton">Log In</button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">
