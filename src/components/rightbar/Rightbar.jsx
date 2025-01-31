9a8dc62b0917f91cd32da3bd5662ac3cff939a90
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-01-31 14:04:27 +0200

    rightbar update

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,33 +1,43 @@
 import React from "react";
 import './rightbar.css';
+import proPic from '../../assets/images/person/9.jpg';
+import proPic2 from '../../assets/images/person/3.jpg';
+import gift from '../../assets/images/gift.jpg';
 
 function Rightbar() {
   return (
     <div className="rightbar">
       <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src={gift} alt=""></img>
+          <span className="birthdayText">
+            <b>Carly Mushkit</b> and <b>3 other friends</b> have a birthday today.
+          </span>
+        </div>
+
         <h4 className="rightbarTitle">Online Friends</h4>
         <ul className="rightbarFriendList">
           <li className="rightbarFriend">
             <div className="rightbarProfileImgContainer">
               <img
                 className="rightbarProfileImg"
-                src="https://via.placeholder.com/40"
+                src={proPic}
                 alt=""
               />
               <span className="rightbarOnline"></span>
             </div>
-            <span className="rightbarUsername">John Doe</span>
+            <span className="rightbarUsername">Rick Sanchez</span>
           </li>
           <li className="rightbarFriend">
             <div className="rightbarProfileImgContainer">
               <img
                 className="rightbarProfileImg"
-                src="https://via.placeholder.com/40"
+                src={proPic2}
                 alt=""
               />
               <span className="rightbarOnline"></span>
             </div>
-            <span className="rightbarUsername">Jane Smith</span>
+            <span className="rightbarUsername">Beth Smith</span>
           </li>
         </ul>
       </div>
