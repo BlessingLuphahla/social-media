1053ec487e5b89c9a0852934ff9ef7dd74c74993
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-01-31 19:50:50 +0200

    profile update

--- src/assets/pages/profile/Profile.jsx
+++ src/assets/pages/profile/Profile.jsx
@@ -5,6 +5,7 @@ import Rightbar from "../../../components/rightbar/Rightbar";
 import "./profile.css";
 
 import profilePic from "../../../assets/images/person/apollo.jpg";
+import coverPic from "../../../assets/images/person/1.jpg";
 
 function Profile() {
   return (
@@ -16,40 +17,19 @@ function Profile() {
         <div className="profileRight">
           <div className="profileRightTop">
             <div className="profileCover">
-              <img className="profileCoverImg" src={profilePic} alt="" />
-              <img
-                className="profileUserImg"
-                src={profilePic}
-                alt=""
-              />
+              <img className="profileCoverImg" src={coverPic} alt="" />
+              <img className="profileUserImg" src={profilePic} alt="" />
             </div>
             <div className="profileInfo">
               <h4 className="profileInfoName">Redd Axe</h4>
               <span className="profileInfoDesc">What it is</span>
             </div>
           </div>
-        </div>
 
-        <div className="profileRightBottom">
-          <div className="profileRightBottomWrapper">
-            <h4 className="profileRightBottomTitle">User information</h4>
-            <div className="profileRightBottomInfo">
-              <div className="profileRightBottomInfoItem">
-                <span className="profileRightBottomInfoKey">City:</span>
-                <span className="profileRightBottomInfoValue">New York</span>
-              </div>
-              <div className="profileRightBottomInfoItem">
-                <span className="profileRightBottomInfoKey">From:</span>
-                <span className="profileRightBottomInfoValue">Madrid</span>
-              </div>
-              <div className="profileRightBottomInfoItem">
-                <span className="profileRightBottomInfoKey">Relationship:</span>
-                <span className="profileRightBottomInfoValue">Single</span>
-              </div>
-            </div>
+          <div className="profileRightBottom">
+            <Feed></Feed>
+            <Rightbar></Rightbar>
           </div>
-          <Feed></Feed>
-          <Rightbar></Rightbar>
         </div>
       </div>
     </>
