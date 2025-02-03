43a17d0514a8e0718acb52f849f15df99f6c4182
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-02-03 23:57:18 +0200

    App update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-
-import Home from './assets/pages/home/Home'
-// import Profile from './assets/pages/profile/Profile'
-// import Login from './assets/pages/login/Login'
+import Home from "./assets/pages/home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./assets/pages/register/Register";
+import Profile from "./assets/pages/profile/Profile";
+import Login from "./assets/pages/login/Login";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/profile/:username" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
