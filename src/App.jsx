44560333a6f8c67f84cccbcc6b1f0fcc9bc85126
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-02-14 11:29:28 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,47 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useContext } from "react";
+import { lazy, Suspense, useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
-import Home from "./assets/pages/home/Home";
-import Register from "./assets/pages/register/Register";
-import Profile from "./assets/pages/profile/Profile";
-import Login from "./assets/pages/login/Login";
-import Messenger from "./components/messenger/Messenger";
+
+const Home = lazy(() => import("./assets/pages/home/Home"));
+const Register = lazy(() => import("./assets/pages/register/Register"));
+const Profile = lazy(() => import("./assets/pages/profile/Profile"));
+const Login = lazy(() => import("./assets/pages/login/Login"));
+const Messenger = lazy(() => import("./components/messenger/Messenger"));
 
 function App() {
   const { user } = useContext(AuthContext);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={user ? <Home /> : <Navigate to="/login" />}
+          />
 
-        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/" /> : <Login />}
+          />
 
-        <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
+          <Route
+            path="/register"
+            element={user ? <Navigate to="/" /> : <Register />}
+          />
 
-        <Route path="/messenger" element={user ? <Messenger /> : <Navigate to="/login" />} />
+          <Route
+            path="/messenger"
+            element={user ? <Messenger /> : <Navigate to="/login" />}
+          />
 
-        <Route path="/profile/:username" element={user ? <Profile /> : <Navigate to="/login" />} />
-      </Routes>
-    </BrowserRouter>
+          <Route
+            path="/profile/:username"
+            element={user ? <Profile /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
